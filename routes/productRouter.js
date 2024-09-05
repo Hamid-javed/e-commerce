914a1b344dce4b2c8654a5458d9be8e509652263
arbab57 <165644659+arbab57@users.noreply.github.com>
2024-09-05 12:51:00 +0500

    added search

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -5,9 +5,9 @@ const { verifyUserToken } = require("../middlewares/authUser");
 
 
 router.get("/categories",verifyUserToken, productControl.getCategories);
-router.get("/featured", productControl.featured);
-router.get("/get/:productId", productControl.getSingle);
-router.get("/search", productControl.search);
+router.get("/featured",verifyUserToken, productControl.featured);
+router.get("/get/:productId",verifyUserToken, productControl.getSingle);
+router.get("/search",verifyUserToken, productControl.search);
 
 
 
