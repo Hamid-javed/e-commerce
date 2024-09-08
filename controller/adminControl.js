a065ee7cf3ce4ba6ad93a2d51c9eaef609c33d39
Hamid <hamidjaved615@gmail.com>
2024-09-08 20:17:23 +0500

    added get and set status of orders APIs

--- controller/adminControl.js
+++ controller/adminControl.js
@@ -8,25 +8,25 @@ exports.addProduct = async (req, res) => {
     try {
         const { name, description, color, size, stock, price, image, category } = req.body;
         if (!name) {
-            return res.status(400).json({ error: "Product name is required." });
+            return res.status(400).json({ message: "Product name is required." });
         }
         if (!description) {
-            return res.status(400).json({ error: "Product description is required." });
+            return res.status(400).json({ message: "Product description is required." });
         }
         if (!color) {
-            return res.status(400).json({ error: "Product color is required." });
+            return res.status(400).json({ message: "Product color is required." });
         }
         if (!size) {
-            return res.status(400).json({ error: "Product size is required." });
+            return res.status(400).json({ message: "Product size is required." });
         }
         if (!stock) {
-            return res.status(400).json({ error: "Product stock is required." });
+            return res.status(400).json({ message: "Product stock is required." });
         }
         if (!price) {
-            return res.status(400).json({ error: "Product price is required." });
+            return res.status(400).json({ message: "Product price is required." });
         }
         if (!image) {
-            return res.status(400).json({ error: "Product image URL is required." });
+            return res.status(400).json({ message: "Product image URL is required." });
         }
         const variant = {
             color: color,
@@ -56,7 +56,49 @@ exports.deleteProdct = async (req, res) => {
             return res.status(404).json({ message: "No product Found!" })
         }
         await Product.findOneAndDelete(product)
-        res.status(200).json({message: "Product deleted successfully!"})
+        res.status(200).json({ message: "Product deleted successfully!" })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+            error: error.message,
+        });
+    }
+}
+
+exports.editProduct = async (req, res) => {
+    try {
+        const { name, description, color, size, stock, price, image, category } = req.body;
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+            error: error.message,
+        });
+    }
+}
+
+exports.getOrders = async (req, res) => {
+    try {
+        const orders = await Order.find();
+        if (!orders) {
+            return res.status(404).json({ message: "No Order Found!" })
+        }
+        res.status(200).json(orders)
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+            error: error.message,
+        });
+    }
+}
+
+exports.orderStatus = async (req, res) => {
+    try {
+        const { status } = req.body;
+        const { productId } = req.params;
+        const order = await Order.findById(productId)
+        order.status = status;
+        order.save()
     } catch (error) {
         res.status(500).json({
             message: "Internal server error",
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,7 +3,10 @@ const adminControl = require("../controller/adminControl")
 
 
 router.post("/add-product", adminControl.addProduct)
+router.post("/edit-product", adminControl.editProduct)
 router.delete("/delete-product/:productId", adminControl.deleteProdct)
+router.get("/get-orders", adminControl.getOrders)
+router.post("/order-placed/:productId", adminControl.orderStatus)
 
 
 module.exports = router;
